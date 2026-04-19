36dd7a80060ef415e8341cbc42e2b749188ae6ba
Ethan Davis <ethandavisecd@gmail.com>
2026-04-18 18:15:28 -0700

    Trace aggregation scripts

---
+++ src/evaluation/validation/aggregate.sql
@@ -0,0 +1,11 @@
+SELECT
+    dataset, pipeline,
+    MIN(ess_bulk_min) AS ess_bulk,
+    MIN(ess_tail_min) AS ess_tail,
+    MAX(r_hat_max) AS r_hat,
+    MAX(mcse_mean_max) AS mcse_mean,
+    MAX(mcse_sd_max) AS mcse_sd,
+    SUM(divergences) AS divergences
+FROM read_csv_auto('{summary_file}')
+GROUP BY dataset, pipeline
+ORDER BY dataset, pipeline
