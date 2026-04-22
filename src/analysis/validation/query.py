548484aef90ab3fd0acdffafe2f6d01e35b7b623
Ethan Davis <ethandavisecd@gmail.com>
2026-04-22 10:17:18 -0700

    Updated analysis validation paths

--- src/analysis/validation/query.py
+++ src/analysis/validation/query.py
@@ -16,132 +16,132 @@ class Query:
 
     def _params(self):
         yield (
-            Path.cwd() / "src" / "analysis" / "validation" / "avg_tmp.sql",
+            Path(__file__).parent / "avg_tmp.sql",
             Path.cwd() / "avg.csv",
             Path.cwd() / "avg_tmp.csv",
         )
         yield (
-            Path.cwd() / "src" / "analysis" / "validation" / "diff_tmp.sql",
+            Path(__file__).parent / "diff_tmp.sql",
             Path.cwd() / "diff.csv",
             Path.cwd() / "diff_tmp.csv",
         )
         yield (
-            Path.cwd() / "src" / "analysis" / "validation" / "auroc" / "freq" / "stats.sql",
+            Path(__file__).parent / "auroc" / "freq" / "stats.sql",
             Path.cwd() / "avg.csv",
             Path.cwd() / "auroc_freq_stats.csv",
         )
         yield (
-            Path.cwd() / "src" / "analysis" / "validation" / "auroc" / "bayes" / "stats.sql",
+            Path(__file__).parent / "auroc" / "bayes" / "stats.sql",
             Path.cwd() / "avg.csv",
             Path.cwd() / "auroc_bayes_stats.csv",
         )
         yield (
-            Path.cwd() / "src" / "analysis" / "validation" / "mcc" / "freq" / "stats.sql",
+            Path(__file__).parent / "mcc" / "freq" / "stats.sql",
             Path.cwd() / "avg.csv",
             Path.cwd() / "mcc_freq_stats.csv",
         )
         yield (
-            Path.cwd() / "src" / "analysis" / "validation" / "mcc" / "bayes" / "stats.sql",
+            Path(__file__).parent / "mcc" / "bayes" / "stats.sql",
             Path.cwd() / "avg.csv",
             Path.cwd() / "mcc_bayes_stats.csv",
         )
         yield (
-            Path.cwd() / "src" / "analysis" / "validation" / "nll" / "freq" / "stats.sql",
+            Path(__file__).parent / "nll" / "freq" / "stats.sql",
             Path.cwd() / "avg.csv",
             Path.cwd() / "nll_freq_stats.csv",
         )
         yield (
-            Path.cwd() / "src" / "analysis" / "validation" / "nll" / "bayes" / "stats.sql",
+            Path(__file__).parent / "nll" / "bayes" / "stats.sql",
             Path.cwd() / "avg.csv",
             Path.cwd() / "nll_bayes_stats.csv",
         )
         yield (
-            Path.cwd() / "src" / "analysis" / "validation" / "brier" / "freq" / "stats.sql",
+            Path(__file__).parent / "brier" / "freq" / "stats.sql",
             Path.cwd() / "avg.csv",
             Path.cwd() / "brier_freq_stats.csv",
         )
         yield (
-            Path.cwd() / "src" / "analysis" / "validation" / "brier" / "bayes" / "stats.sql",
+            Path(__file__).parent / "brier" / "bayes" / "stats.sql",
             Path.cwd() / "avg.csv",
             Path.cwd() / "brier_bayes_stats.csv",
         )
         yield (
-            Path.cwd() / "src" / "analysis" / "validation" / "ece" / "freq" / "stats.sql",
+            Path(__file__).parent / "ece" / "freq" / "stats.sql",
             Path.cwd() / "avg.csv",
             Path.cwd() / "ece_freq_stats.csv",
         )
         yield (
-            Path.cwd() / "src" / "analysis" / "validation" / "ece" / "bayes" / "stats.sql",
+            Path(__file__).parent / "ece" / "bayes" / "stats.sql",
             Path.cwd() / "avg.csv",
             Path.cwd() / "ece_bayes_stats.csv",
         )
         yield (
-            Path.cwd() / "src" / "analysis" / "validation" / "mce" / "freq" / "stats.sql",
+            Path(__file__).parent / "mce" / "freq" / "stats.sql",
             Path.cwd() / "avg.csv",
             Path.cwd() / "mce_freq_stats.csv",
         )
         yield (
-            Path.cwd() / "src" / "analysis" / "validation" / "mce" / "bayes" / "stats.sql",
+            Path(__file__).parent / "mce" / "bayes" / "stats.sql",
             Path.cwd() / "avg.csv",
             Path.cwd() / "mce_bayes_stats.csv",
         )
         yield (
-            Path.cwd() / "src" / "analysis" / "validation" / "auroc" / "freq" / "bins.sql",
+            Path(__file__).parent / "auroc" / "freq" / "bins.sql",
             Path.cwd() / "avg.csv",
             Path.cwd() / "auroc_freq_bins.csv",
         )
         yield (
-            Path.cwd() / "src" / "analysis" / "validation" / "auroc" / "bayes" / "bins.sql",
+            Path(__file__).parent / "auroc" / "bayes" / "bins.sql",
             Path.cwd() / "avg.csv",
             Path.cwd() / "auroc_bayes_bins.csv",
         )
         yield (
-            Path.cwd() / "src" / "analysis" / "validation" / "mcc" / "freq" / "bins.sql",
+            Path(__file__).parent / "mcc" / "freq" / "bins.sql",
             Path.cwd() / "avg.csv",
             Path.cwd() / "mcc_freq_bins.csv",
         )
         yield (
-            Path.cwd() / "src" / "analysis" / "validation" / "mcc" / "bayes" / "bins.sql",
+            Path(__file__).parent / "mcc" / "bayes" / "bins.sql",
             Path.cwd() / "avg.csv",
             Path.cwd() / "mcc_bayes_bins.csv",
         )
         yield (
-            Path.cwd() / "src" / "analysis" / "validation" / "nll" / "freq" / "bins.sql",
+            Path(__file__).parent / "nll" / "freq" / "bins.sql",
             Path.cwd() / "avg.csv",
             Path.cwd() / "nll_freq_bins.csv",
         )
         yield (
-            Path.cwd() / "src" / "analysis" / "validation" / "nll" / "bayes" / "bins.sql",
+            Path(__file__).parent / "nll" / "bayes" / "bins.sql",
             Path.cwd() / "avg.csv",
             Path.cwd() / "nll_bayes_bins.csv",
         )
         yield (
-            Path.cwd() / "src" / "analysis" / "validation" / "brier" / "freq" / "bins.sql",
+            Path(__file__).parent / "brier" / "freq" / "bins.sql",
             Path.cwd() / "avg.csv",
             Path.cwd() / "brier_freq_bins.csv",
         )
         yield (
-            Path.cwd() / "src" / "analysis" / "validation" / "brier" / "bayes" / "bins.sql",
+            Path(__file__).parent / "brier" / "bayes" / "bins.sql",
             Path.cwd() / "avg.csv",
             Path.cwd() / "brier_bayes_bins.csv",
         )
         yield (
-            Path.cwd() / "src" / "analysis" / "validation" / "ece" / "freq" / "bins.sql",
+            Path(__file__).parent / "ece" / "freq" / "bins.sql",
             Path.cwd() / "avg.csv",
             Path.cwd() / "ece_freq_bins.csv",
         )
         yield (
-            Path.cwd() / "src" / "analysis" / "validation" / "ece" / "bayes" / "bins.sql",
+            Path(__file__).parent / "ece" / "bayes" / "bins.sql",
             Path.cwd() / "avg.csv",
             Path.cwd() / "ece_bayes_bins.csv",
         )
         yield (
-            Path.cwd() / "src" / "analysis" / "validation" / "mce" / "freq" / "bins.sql",
+            Path(__file__).parent / "mce" / "freq" / "bins.sql",
             Path.cwd() / "avg.csv",
             Path.cwd() / "mce_freq_bins.csv",
         )
         yield (
-            Path.cwd() / "src" / "analysis" / "validation" / "mce" / "bayes" / "bins.sql",
+            Path(__file__).parent / "mce" / "bayes" / "bins.sql",
             Path.cwd() / "avg.csv",
             Path.cwd() / "mce_bayes_bins.csv",
         )
